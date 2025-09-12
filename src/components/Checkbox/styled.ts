e564a31be08b9fad6832155e84d5b20e8cc749b0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-09-12 17:52:29 -0300

    ✨ feat: cria estilos para componente Checkbox
    Adiciona `Box`, `Label` e `TextoErro` com estilos básicos para estruturar o checkbox e exibir mensagens de erro.

---
+++ src/components/Checkbox/styled.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+export const Box = styled.div`
+    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+    margin: 16px 0;
+`
+
+export const Label = styled.label`
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 24px;
+`
+
+export const TextoErro = styled.div`
+    color: red;
+    margin-top: 4px;
+`
